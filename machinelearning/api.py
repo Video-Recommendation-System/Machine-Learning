92a086d372279903524bf2fbf1490038a0ba1927
Christopher Wells <cwellsny@nycap.rr.com>
2018-01-28 08:37:39 -0500

    Make misc improvements

--- machinelearning/api.py
+++ machinelearning/api.py
@@ -1,18 +1,23 @@
 from __future__ import print_function
 from clustering import *
-from flask import Flask, request
+from flask import Flask, request, render_template
 from google.cloud import language
 
+import json
 import pandas as pd
 import yapi
 
 import features
 import youtube
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 app = Flask(__name__)
 
 api_key_file = "key.txt"
-data_file = "csv2.csv"
+data_file = "../feature-extractor/output.csv"
 
 api_key = youtube.readfile(api_key_file)[0]
 ytapi = yapi.YoutubeAPI(api_key)
@@ -33,10 +38,21 @@ def recommendations():
     vector = get_video_vector(last_video)
     print(vector)
     recommended_video_ids = predict(data, model, vector)
-    return get_video_info(recommended_video_ids, data)
+    return str(get_video_info(recommended_video_ids, data))
+
+@app.route("/<video>")
+def video_rec(video):
+    vector = get_video_vector(video)
+    recommended_video_ids = predict(data, model, vector)
+    recommended = get_video_info(recommended_video_ids, data)
+
+    print(recommended)
+
+    return render_template("video.html", videos=recommended)
 
 def get_video_vector(video_id):
     video = youtube.grab_video(ytapi, video_id)
+    print(video)
     info = youtube.get_video_info(video_id, video)
 
     return np.array(features.get_video_info(client, info))[6:]
@@ -47,11 +63,11 @@ def get_video_info(video_ids, data):
         entry = data[data["video_id"] == video_id].iloc[0]
         entries.append({
             "video_url": "https://www.youtube.com/watch?v=" + entry["video_id"],
-            "thumbnail_url": entry["thumbnail"],
+            "thumbnail_url": "https://i.ytimg.com/vi/" + entry["video_id"] + "/hqdefault.jpg",
             "video_title": entry["video_title"]
             })
 
-    return str(entries)
+    return entries
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8800)
